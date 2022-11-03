5afa86db5659e0ae41b25d8c1ea5068785658481
Senjerak <gabriela.lecusay001@mymdc.net>
2022-11-02 21:16:05 -0400

    jsAttempt

---
+++ js/caps.js
@@ -0,0 +1,10 @@
+const divClose = document.querySelector('.overlay');
+const buttonClose = document.querySelector('#closeup')
+
+
+// function myFunction0(){
+
+//     alert('hewwo!')
+// }
+
+buttonClose.addEventListener('click', () => divClose.style.display = 'none');
