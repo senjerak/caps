7217847431f65aeacc0ed6bfc9c2fec3273d35f3
gabi <lecusay.gabriela@gmail.com>
2023-02-13 13:42:29 -0500

    fix-up
    Co-authored-by: Angel J Piscola <Redmega@users.noreply.github.com>

--- js/caps.js
+++ js/caps.js
@@ -10,64 +10,85 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const spoinkClose = document.querySelectorAll('.spoinkbob button');
     const popUp = document.querySelector('.overlay');
     const buttonClose = document.querySelector('#closeup')
+    const scene = document.querySelector('#scene')
+    const grassScene = scene.querySelector('.grass-scene')
     buttonClose.addEventListener('click', function () {
         popUp.style.opacity = 0;
         setTimeout(() => popUp.remove(), 1000);
-        ding.volme = 0.5;
+        ding.volume = 0.5;
         BGmusic.volume = 0.1;
+        spoinkMusic.volume = 0.5;
         ding.play();
-        ding.addEventListener('ended', () => {
+        setTimeout(() => {
             BGmusic.play();
-        })
-        spoinkClose.forEach((button) => {
-            button.style.opacity = 1;
-        })
+            spoinkClose.forEach((button) => {
+                button.style.opacity = 1;
+            })
+        }, 1000)
     });
 
-    const caps = Array.from(document.querySelectorAll('.cap'));
+    const caps = []
 
-    // caps size 
-    for (const cap of caps) {
+    // cap factory
+    function makeCap() {
         const offset = Math.max(0.5, Math.random() * 1.25)
-        const capSize = offset * 155;
-        cap.style.width = `${capSize}px`;
-        cap.style.height = `${capSize}px`;
 
-        cap.style.setProperty('--size-offset', capSize)
+        const size = offset * 155
+
+        const speed = Math.floor(Math.random() * 8 + 1.5);
+
+        const species = Math.floor(Math.random() * 3 + 1);
+
+        const cap = { id: caps.length, species: species, size: size, speed: speed }
+
+        return cap
     }
 
-    // big slow small fast
-    caps.sort((a, b) => a.style.getPropertyValue('--size-offset') - b.style.getPropertyValue('--size-offset'))
-    caps[0].classList.add('fast')
-    caps[caps.length - 1].classList.add('slow')
-
-    caps.forEach((cap, i) => {
-        cap.style.zIndex = caps.length - i;
-    })
-
-    // follow meeeeee
-    document.addEventListener("mousemove", _.throttle(function mauscap(e) {
-        caps.forEach(cap => {
-            cap.style.left = e.clientX + "px";
-            const bounds = cap.getBoundingClientRect();
-            const center = (bounds.left + bounds.right) / 2;
-            if (center > e.clientX) cap.classList.add("flip");
-            else cap.classList.remove("flip");
-        })
-    }, 1000))
-
     //spoinky timer
-
     spoinkClose.forEach((button => {
-        button.addEventListener('click', function showCap() {
+        button.addEventListener('click', function birthCap() {
+            const bounds = this.getBoundingClientRect();
+            spoinkMusic.currentTime = 0;
             spoinkMusic.play();
             this.style.opacity = 0;
-            const cap = document.getElementById(this.dataset.target);
-            cap.style.opacity = 1;
+            this.style.pointerEvents = 'none';
+
+            const cap = makeCap();
+            
+            const image = document.createElement('img');
+            image.id=`cap-${cap.id}`
+            image.className = 'cap'
+            image.src = `./images/walkies${cap.species}.gif`
+            image.style.width = `${cap.size}px`
+            image.style.height = `${cap.size}px`
+            image.style.left = bounds.left + "px";
+            console.log('left is ', bounds.left)
+            image.style.setProperty('--speed',`${cap.speed}s`)
+            image.style.setProperty('--size-offset', cap.size)
+            
+            document.addEventListener('mousemove', _.throttle(function mauscap(e) {
+                image.style.left = e.clientX + "px";
+                const bounds = image.getBoundingClientRect();
+                const center = (bounds.left + bounds.right) / 2;
+                if (center > e.clientX) image.classList.add("flip");
+                else image.classList.remove("flip");
+            }, 250))
+            
+            
+            scene.insertBefore(image, grassScene)
+
+            caps.push(cap)
+            caps.sort((a,b) => b.size - a.size)
+            caps.forEach((cap,i) => {
+                document.getElementById(`cap-${cap.id}`).style.zIndex = i
+            })
+
+            const returnTimer = Math.floor(Math.random() * 10 + 2);
+            console.log('return timer', returnTimer)
+            setTimeout(() => {
+                this.style.opacity = 1;
+                this.style.pointerEvents = 'auto'
+            }, returnTimer * 1000)
         })
     }))
-
-
 });
-
-
---
+++ notes.md
@@ -0,0 +1,13 @@
+# Caps plan
+- [x] have spoinks reappear
+  - random timeout between 5-20 seconds
+- [x] spoinks spawn caps
+  - spoinks spawn random cap
+- [x] individual cap species can vary in size and speed
+- [x] fix range of sensitivity to mouse location on screen
+
+
+# Assets
+- Caps Still
+- Caps 4 & 5
+- Star Animation
